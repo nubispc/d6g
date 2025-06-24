e3f50bd5a2849e62227434c80c25ce71e582b161
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2025-06-24 10:53:05 +0300

    fix: Fix Service Orchestrator integration bugs

--- components/service-orchestrator/src/app.py
+++ components/service-orchestrator/src/app.py
@@ -7,6 +7,8 @@ import base64
 import os
 import re
 from library.messaging import get_message_client
+import json
+import yaml
 
 client = get_message_client()
 tags_metadata = [
@@ -68,35 +70,59 @@ async def deploy_service(service_request: ServiceRequest = Body(...)):
         iml_endpoint = "http://localhost:5000"  # Default value if not found
 
     response_content = await client.receive_message()
+    response_content=interpret_message(response_content)
     if response_content:
-        # print(f"Received final message: {response_content}")
+        print(f"Received final message: {response_content}")
         request_states[request_id]["status"] = "completed"
         request_states[request_id]["output"] = response_content
-
-        # Convert the string content to a file-like object
-        file_like_object = BytesIO(base64.b64decode(
-            response_content).decode().encode('utf-8'))
+        if "Error" in response_content:
+            request_states[request_id]["status"] = "failed"
+            return JSONResponse(content={"message": "Error in Optimization Engine", "status": "failed",
+                                         "error": f"Optimization Engine error: {response_content["Error"]}"})
+        if "Failed" in response_content:
+            request_states[request_id]["status"] = "failed"
+            return JSONResponse(content={"message": "Failure in Optimization Engine", "status": "failed",
+                                         "error": f"Optimization Engine failure: {response_content["Failed"]}"})
+        yaml_file_content = yaml.dump(response_content).encode('utf-8')
+        
+        # Convert the string content to a file-like object.
+        # TODO: This line caused errors. Perhaps we need to remove it.
+        # Also the b64 decode step is not needed at the moment.
+        # file_like_object = BytesIO(base64.b64decode(response_content).decode().encode('utf-8'))
+        
+        file_like_object = BytesIO(yaml_file_content)
+        
         files = {'file': ('demo_nsd.yml', file_like_object)}
+        
+        try:
+            # The timeout tuple is set to (0.5, 10) seconds for connection and read timeouts respectively.
+            # TODO: When integrating with IML, we need to increase the connection timeout.
+            iml_response = requests.post(f"{iml_endpoint}", files=files, timeout=(0.5, 10))
+            # print(iml_response.json())
+            # import pdb;pdb.set_trace()
 
-        iml_response = requests.post(f"{iml_endpoint}", files=files)
-        # print(iml_response.json())
-        # import pdb;pdb.set_trace()
+            json_data = iml_response.json()['response']
+            # Use a regular expression to find key-value pairs
+            pattern = r'(\w+):\s*([^,}]+)'
+            matches = re.findall(pattern, json_data)
 
-        json_data = iml_response.json()['response']
-        # Use a regular expression to find key-value pairs
-        pattern = r'(\w+):\s*([^,}]+)'
-        matches = re.findall(pattern, json_data)
+            # Convert the matches to a dictionary
+            structured_dict = {key: value for key, value in matches}
 
-        # Convert the matches to a dictionary
-        structured_dict = {key: value for key, value in matches}
+            service_id = int(structured_dict["id"])
+            service_name = structured_dict["Deployed"]
+            deployed_services[service_id] = {
+                "status": "deployed", "service_name": service_name, "file_name": file_name, "site_id": site_id, "iml_endpoint": iml_endpoint}
 
-        service_id = int(structured_dict["id"])
-        service_name = structured_dict["Deployed"]
-        deployed_services[service_id] = {
-            "status": "deployed", "service_name": service_name, "file_name": file_name, "site_id": site_id, "iml_endpoint": iml_endpoint}
-
-        # return JSONResponse(content={"message": "Received", "data": iml_response.json(), "file": base64.b64decode(response_content).decode(), "site_id": site_id, "service_id": service_id })
-        return JSONResponse(content={"message": "Received", "status": "deployed", "service_name": service_name, "file_name": file_name, "site_id": site_id, "iml_endpoint": iml_endpoint})
+            # return JSONResponse(content={"message": "Received", "data": iml_response.json(), "file": base64.b64decode(response_content).decode(), "site_id": site_id, "service_id": service_id })
+            return JSONResponse(content={"message": "Received", "status": "deployed", "service_name": service_name,
+                                         "file_name": file_name, "site_id": site_id, "iml_endpoint": iml_endpoint})
+        except:
+            request_states[request_id]["status"] = "failed"
+        return JSONResponse(content={"message": "Failed to deploy service to IML", "status": "failed",
+                                     "service_name": service_request.name, "site_id": site_id,
+                                     "iml_endpoint": iml_endpoint,
+                                     "requested_service": response_content})
     else:
         print("No final message received.")
         request_states[request_id]["status"] = "failed"
@@ -131,25 +157,52 @@ async def delete_service(service_id: str):
 
 @app.get("/requests", tags=["requests"])
 async def get_requests():
-    return JSONResponse(content={"deployed_services": list(request_states.keys())})
+    return JSONResponse(content={"requested_services": list(request_states.keys())})
 
 
 @app.get("/requests/{request_id}", tags=["requests"])
-async def get_request_by_id(request_id: str):
+async def get_request_by_id(request_id: int):
     if request_id not in request_states:
-        raise HTTPException(status_code=404, detail="Service not found")
-    return JSONResponse(content={"service_name": request_id, "details": request_states[request_id]})
+        raise HTTPException(status_code=404, detail="Request not found")
+    return JSONResponse(content={"request": request_id, "details": request_states[request_id]})
 
 
 def download_file_from_catalog(file_name: str) -> str | None:
     url = f'{SERVICE_CATALOG_URL}/retrieve/{file_name}'
-    response = requests.get(url)
+    print(f"Downloading file from {url}")
+    try:
+        response = requests.get(url)
+        print(f"Response status code: {response.status_code}")
+        response.raise_for_status()
+    except:
+        print(f"Error downloading file: {response.text}")
+        return None
     if response.status_code == 200:
         return response.json()["file_content"]
     else:
         print(f"Failed to download file: {response.text}")
         return None
 
+def interpret_message(message):
+    if message is None:
+        return None
+    try:
+        message = base64.b64decode(message).decode('utf-8')
+    except Exception:
+        pass
+    msg = ""
+    try:
+        msg = json.loads(message)
+    except:
+        pass
+    try:
+        msg = yaml.safe_load(message)
+    except:
+        pass
+    if isinstance(msg, str):
+        if msg == "":
+            return None
+    return msg
 
 if __name__ == "__main__":
     import uvicorn
